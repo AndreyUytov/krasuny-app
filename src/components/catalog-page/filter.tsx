518a9cefda2b405acd9e2a356767c4d5a7885d3b
Andrew <uytov1991@gmail.com>
2020-07-10 15:18:54 +0300

    в догонку переделываю каталог-сраницу

--- src/components/catalog-page/filter.tsx
+++ src/components/catalog-page/filter.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { TAG_LIST, PRODUCT_TYPE } from '../../types'
 
-const FilterCatalogPage: React.FC = () => {
+interface IFilterCatalogPage {
+  tags: TAG_LIST[],
+  product_type: PRODUCT_TYPE
+}
+
+const FilterCatalogPage: React.FC<IFilterCatalogPage> = ({tags, product_type}) => {
   return (
     <section className = "filter-block">
-      Filter
+      Тэги: {tags}
+      Тип продукта: {product_type} 
     </section>
   )
 }
--- src/components/catalog-page/index.tsx
+++ src/components/catalog-page/index.tsx
@@ -4,15 +4,16 @@ import {connect, ConnectedProps} from 'react-redux'
 import MainNav from './../main-nav/'
 import FilterCatalogPage from './filter'
 import { RootState } from '../../reducers'
-import { PRODUCT_TYPE } from '../../types'
+import { PRODUCT_TYPE, TAG_LIST } from '../../types'
 
 interface ICatalogPage {
-  product_type: PRODUCT_TYPE
+  product_type: PRODUCT_TYPE,
+  tags: TAG_LIST[]
 }
 
 const mapStateToProps = (state: RootState) => ({
   product_type: state.filters.selectedProductType,
-
+  tags: state.filters.selectedTags
 })
 
 const connector = connect(mapStateToProps)
@@ -25,7 +26,7 @@ const CatalogPage:React.FC<CatalogPageType> = (props) => {
   return (
     <main className = "page-main--catalog container">
       <MainNav links = {['Главная']} to = {['/']} {...props} />
-      <FilterCatalogPage />
+      <FilterCatalogPage {...props}/>
     </main>
   )
 }
