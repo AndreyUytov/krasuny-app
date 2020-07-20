e95eb1bfc251e13f3b89c421221ad56fc82ef929
Andrew <uytov1991@gmail.com>
2020-07-20 15:32:19 +0300

    добавил обработку инпутов на цене

--- src/components/catalog-page/filter.tsx
+++ src/components/catalog-page/filter.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { BrandsList, BRANDS } from '../../types'
+import { BrandsList, BRANDS, MIN_DELTA_PRICE } from '../../types'
 
 interface IFilterCatalogPage {
   brand: BrandsList | undefined
@@ -8,6 +8,43 @@ interface IFilterCatalogPage {
 const FilterCatalogPage: React.FC<IFilterCatalogPage> = ({brand}) => {
 
   const [checkedBrand, setCheckedBrand] = useState<BrandsList | undefined>(brand)
+  const [minTooglePosition, setMinTooglePosition] = useState(10)
+  const [maxTooglePosition, setMaxTooglePosition] = useState(10)
+  const [valueMinPrice, setValueMinPrice] = useState(10)
+  const [valueMaxPrice, setValueMaxPrice] = useState(90)
+
+  const onMinInputChange = (value: number) => {
+    if (value <= valueMaxPrice - MIN_DELTA_PRICE) {
+      setMinTooglePosition(value)
+    } else if (value >= valueMaxPrice) {
+      alert(`Минимальная цена не может быть больше или равной максимальной`)
+      setMaxTooglePosition(100 - value)
+      setValueMaxPrice(value)
+      setMinTooglePosition(value - MIN_DELTA_PRICE)
+      setValueMinPrice(value - MIN_DELTA_PRICE)
+    } else {
+      alert(`Дельта диапозона цен должна быть не менее ${MIN_DELTA_PRICE} единиц`)
+      setMinTooglePosition(valueMaxPrice - MIN_DELTA_PRICE)
+      setValueMinPrice(valueMaxPrice - MIN_DELTA_PRICE)
+    }
+  }
+
+  const onMaxInputChange = (value: number) => {
+    if (value >= valueMinPrice + MIN_DELTA_PRICE) {
+      setMaxTooglePosition(100 - value)
+    } else if (value <= valueMinPrice) {
+      alert (`Максимальная цена диапазона не может быть меньше или равной минимальной`)
+      setMinTooglePosition(value)
+      setValueMinPrice(value)
+      setMaxTooglePosition(100 - (value + MIN_DELTA_PRICE))
+      setValueMaxPrice(value + MIN_DELTA_PRICE)
+    } else {
+      alert(`Дельта диапозона цен должна быть не менее ${MIN_DELTA_PRICE} единиц`)
+      setMaxTooglePosition(100 - (valueMinPrice + MIN_DELTA_PRICE))
+      setValueMaxPrice(valueMinPrice + MIN_DELTA_PRICE)
+    }
+  }
+
 
   return (
     <section className = "filter-block">
@@ -18,14 +55,10 @@ const FilterCatalogPage: React.FC<IFilterCatalogPage> = ({brand}) => {
         <fieldset className="filter-form__price filter-form__fieldset">
           <div className="filter-form__price-top-wrapper">
             <legend className="filter-form__legend">Цена</legend>
-            <label className="filter-form__label">
-              <input className="visually-hidden" type="checkbox" name="price-check" />
-              <span className="filter-form__checkbox checkbox"></span> Скидки
-            </label>
           </div>
           <div className="wrapper-price-range">
             <div className="price-range__scale">
-              <div className="price-range__bar">
+              <div className="price-range__bar" style={{left: `${minTooglePosition}%`, right: `${maxTooglePosition}%`}} >
                 <div className="price-range__toggle price-range__toggle--min"></div>
                 <div className="price-range__toggle price-range__toggle--max"></div>
               </div>
@@ -33,11 +66,11 @@ const FilterCatalogPage: React.FC<IFilterCatalogPage> = ({brand}) => {
             <div className="wrapper-price-input">
               <label className="filter-form__label">
                 <span className="visually-hidden">min price</span>
-                <input className="filter-form__input" type="text" value="930" />
+                <input className="filter-form__input" type="text" value={valueMinPrice} onChange = {(evt) => {setValueMinPrice(+evt.currentTarget.value)}} onBlur = {(evt) => onMinInputChange(+evt.currentTarget.value)} />
               </label>
               <label className="filter-form__label">
                 <span className="visually-hidden">max price</span>
-                <input className="filter-form__input" type="text" value="25093" />
+                <input className="filter-form__input" type="text" value={valueMaxPrice} onChange = {(evt) => setValueMaxPrice(+evt.currentTarget.value)} onBlur = {(evt) => onMaxInputChange(+evt.currentTarget.value)} />
               </label>
             </div>
           </div>
--- src/types/index.ts
+++ src/types/index.ts
@@ -43,6 +43,8 @@ export const MAX_ITEMS_PER_PAGE = 16
 export const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE'
 export const RESET_CURRENT_PAGE = 'RESET_CURRENT_PAGE'
 
+export const MIN_DELTA_PRICE = 10
+
 export interface SetCurrentPageAction {
   type: typeof SET_CURRENT_PAGE,
   page: number
