fbee45ed0c4210ba88678d5cefcef85cfcc83ae4
Андрей <uytov1991@gmail.com>
2021-05-29 18:36:44 +0300

    Updates

---
+++ precache-manifest.a5fa18f2569ee4582ca75cfdc8f5a5c5.js
@@ -0,0 +1,406 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf18662791ce106d41952a006c1bc1f0",
+    "url": "/krasuny-app/index.html"
+  },
+  {
+    "revision": "0732038efc5e9f4e38cc",
+    "url": "/krasuny-app/static/css/main.cb3e7d76.chunk.css"
+  },
+  {
+    "revision": "7a7dee031c484a4831ae",
+    "url": "/krasuny-app/static/js/2.ce088e30.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/krasuny-app/static/js/2.ce088e30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0732038efc5e9f4e38cc",
+    "url": "/krasuny-app/static/js/main.c2c70aa5.chunk.js"
+  },
+  {
+    "revision": "77cfcb50a237bf3974c6",
+    "url": "/krasuny-app/static/js/runtime-main.a3990148.js"
+  },
+  {
+    "revision": "a6c4f668d347feb5ad6e9c4e7a162da8",
+    "url": "/krasuny-app/static/media/1-catalog.a6c4f668.png"
+  },
+  {
+    "revision": "e602c6fcd38cd62d70bc0570e6667204",
+    "url": "/krasuny-app/static/media/1-nav.e602c6fc.png"
+  },
+  {
+    "revision": "5919944765bba097211881383251146c",
+    "url": "/krasuny-app/static/media/10-catalog.59199447.png"
+  },
+  {
+    "revision": "03d252502fbcae8696e19306717fee54",
+    "url": "/krasuny-app/static/media/2-catalog.03d25250.png"
+  },
+  {
+    "revision": "b0aa08b8079fc16be2d817383c7f6ef3",
+    "url": "/krasuny-app/static/media/2-nav.b0aa08b8.png"
+  },
+  {
+    "revision": "e18e0c423e47d7c4307737fdb8ae6c8f",
+    "url": "/krasuny-app/static/media/3-catalog.e18e0c42.png"
+  },
+  {
+    "revision": "18798b5afdb1602f3cefd985e253c08d",
+    "url": "/krasuny-app/static/media/4-catalog.18798b5a.png"
+  },
+  {
+    "revision": "c1b0c1f1525078817ad5e72397ecc73f",
+    "url": "/krasuny-app/static/media/5-catalog.c1b0c1f1.png"
+  },
+  {
+    "revision": "7bc4eb2cbea067c1c9b53fa9f86ee878",
+    "url": "/krasuny-app/static/media/6-catalog.7bc4eb2c.png"
+  },
+  {
+    "revision": "51df9397896467259d5a20bb22ff1f1e",
+    "url": "/krasuny-app/static/media/6-nav.51df9397.png"
+  },
+  {
+    "revision": "5f95958055a8241ca3e284699f95b81a",
+    "url": "/krasuny-app/static/media/7-catalog.5f959580.png"
+  },
+  {
+    "revision": "fa9c8ee67f0cb2abed3d365ff630fb09",
+    "url": "/krasuny-app/static/media/8-catalog.fa9c8ee6.png"
+  },
+  {
+    "revision": "1b39faf6d657a5c48e30af900566e960",
+    "url": "/krasuny-app/static/media/9-catalog.1b39faf6.png"
+  },
+  {
+    "revision": "f88015d4bb9c2d908cc7dc73111b1b4b",
+    "url": "/krasuny-app/static/media/Maestro.f88015d4.svg"
+  },
+  {
+    "revision": "227a4c002ff24f610bc55b9aa8520de7",
+    "url": "/krasuny-app/static/media/MasterCard.227a4c00.svg"
+  },
+  {
+    "revision": "4d3037ac5c8002b95bf489245f6ad11e",
+    "url": "/krasuny-app/static/media/PayPal.4d3037ac.svg"
+  },
+  {
+    "revision": "85441a87d5e31de31d9052d3a229ff9d",
+    "url": "/krasuny-app/static/media/Visa.85441a87.svg"
+  },
+  {
+    "revision": "ca1e12cff2351c1b7c67763596d10abc",
+    "url": "/krasuny-app/static/media/about-block-fon.ca1e12cf.png"
+  },
+  {
+    "revision": "192da8e90d1bbe1efd3da242346b8029",
+    "url": "/krasuny-app/static/media/add-snap--hover.192da8e9.svg"
+  },
+  {
+    "revision": "fb1b3a727555ac33c668adeb0e85fd7c",
+    "url": "/krasuny-app/static/media/add-snap.fb1b3a72.svg"
+  },
+  {
+    "revision": "478a515fcd8c8dc638cfe7320f56e1d9",
+    "url": "/krasuny-app/static/media/article-1.478a515f.png"
+  },
+  {
+    "revision": "bd211915315e65beaf2cc209ec391c78",
+    "url": "/krasuny-app/static/media/article-2.bd211915.png"
+  },
+  {
+    "revision": "2ec8c47a13c44c6089b7bae27f859920",
+    "url": "/krasuny-app/static/media/article-3.2ec8c47a.png"
+  },
+  {
+    "revision": "330db7bff457cb01dfd2444df6be1e44",
+    "url": "/krasuny-app/static/media/article-4.330db7bf.png"
+  },
+  {
+    "revision": "5c02c268071725b3a9583b09d37882ba",
+    "url": "/krasuny-app/static/media/banner-big-img.5c02c268.png"
+  },
+  {
+    "revision": "6030ad2bd7f6a664037b0c51260ce01b",
+    "url": "/krasuny-app/static/media/banner-fon.6030ad2b.png"
+  },
+  {
+    "revision": "8373b36c175f3d4c3a6f559849404f10",
+    "url": "/krasuny-app/static/media/banner-small-img.8373b36c.png"
+  },
+  {
+    "revision": "f9eb2a545d0c8bf274b020e5696351b1",
+    "url": "/krasuny-app/static/media/basket-snap.f9eb2a54.svg"
+  },
+  {
+    "revision": "fa137895762a75490a90b98026d9467f",
+    "url": "/krasuny-app/static/media/breadcrumbs-fon--active.fa137895.svg"
+  },
+  {
+    "revision": "f1c3dcfb50b175581166afbf6682d648",
+    "url": "/krasuny-app/static/media/breadcrumbs-fon.f1c3dcfb.svg"
+  },
+  {
+    "revision": "e8e094c21eafe56fcf83114d98a13c2c",
+    "url": "/krasuny-app/static/media/cash.e8e094c2.svg"
+  },
+  {
+    "revision": "2258c83e3e24b4920942e4c428404157",
+    "url": "/krasuny-app/static/media/checkbox--checked.2258c83e.svg"
+  },
+  {
+    "revision": "1d5614039608d4c66e07cf658a572abb",
+    "url": "/krasuny-app/static/media/checkbox--hover.1d561403.svg"
+  },
+  {
+    "revision": "acfcab23e5c6afe3b9cc9b032b26d92b",
+    "url": "/krasuny-app/static/media/checkbox.acfcab23.svg"
+  },
+  {
+    "revision": "862d9e417605daf1528c6d6fd717b1b9",
+    "url": "/krasuny-app/static/media/close-snap--hover.862d9e41.svg"
+  },
+  {
+    "revision": "8f9c63c5c0b0985f3baefc7f0dae435e",
+    "url": "/krasuny-app/static/media/close-snap.8f9c63c5.svg"
+  },
+  {
+    "revision": "28e8022ab44df52a4b9d924872f8a0ff",
+    "url": "/krasuny-app/static/media/favorite-snap--on.28e8022a.svg"
+  },
+  {
+    "revision": "c237fdee9db19d4b3c44be04dc39641f",
+    "url": "/krasuny-app/static/media/favorite-snap.c237fdee.svg"
+  },
+  {
+    "revision": "abb1560f48d2332f68eac645f43748ef",
+    "url": "/krasuny-app/static/media/info-marker.abb1560f.svg"
+  },
+  {
+    "revision": "f5abc05b3d97e1831b5b444f582f25cc",
+    "url": "/krasuny-app/static/media/instagram-img.f5abc05b.png"
+  },
+  {
+    "revision": "5b2b2bea8002c5ac1bb02d60f5faddc1",
+    "url": "/krasuny-app/static/media/latoblack.5b2b2bea.woff"
+  },
+  {
+    "revision": "67a4543adcb77e4c7796a6844fa188b7",
+    "url": "/krasuny-app/static/media/latoblack.67a4543a.woff2"
+  },
+  {
+    "revision": "46380352796e5966f0d0991c91eec0ca",
+    "url": "/krasuny-app/static/media/latoblackitalic.46380352.woff"
+  },
+  {
+    "revision": "a0894cb3424a7279b441a7edc7a60565",
+    "url": "/krasuny-app/static/media/latoblackitalic.a0894cb3.woff2"
+  },
+  {
+    "revision": "7ff9916172d5bdd2dd14a28f890ec83a",
+    "url": "/krasuny-app/static/media/latobold.7ff99161.woff"
+  },
+  {
+    "revision": "e1cbc7aaea1cb5f65dd77ffdc5126556",
+    "url": "/krasuny-app/static/media/latobold.e1cbc7aa.woff2"
+  },
+  {
+    "revision": "0e3cc35ec935cd8df38489d3c4fe138f",
+    "url": "/krasuny-app/static/media/latobolditalic.0e3cc35e.woff2"
+  },
+  {
+    "revision": "432c5130ceb6e5aee63661ed6032f6af",
+    "url": "/krasuny-app/static/media/latobolditalic.432c5130.woff"
+  },
+  {
+    "revision": "4eeb1666fa62ad4b00ed957fe217739f",
+    "url": "/krasuny-app/static/media/latohairline.4eeb1666.woff"
+  },
+  {
+    "revision": "acd7be8835e508b6f943093a76ce20ab",
+    "url": "/krasuny-app/static/media/latohairline.acd7be88.woff2"
+  },
+  {
+    "revision": "8efde0150a3421cd492c0690eb3aeabc",
+    "url": "/krasuny-app/static/media/latohairlineitalic.8efde015.woff2"
+  },
+  {
+    "revision": "ea87665a2c556a1c0d4a74220b9823ed",
+    "url": "/krasuny-app/static/media/latohairlineitalic.ea87665a.woff"
+  },
+  {
+    "revision": "37bbdbcbc1b26577f0a009fdf34f060a",
+    "url": "/krasuny-app/static/media/latoheavy.37bbdbcb.woff2"
+  },
+  {
+    "revision": "3c8e2fea9560cc782d239c52c81faeb0",
+    "url": "/krasuny-app/static/media/latoheavy.3c8e2fea.woff"
+  },
+  {
+    "revision": "915671b2e7a29a4deb2731a569b89d46",
+    "url": "/krasuny-app/static/media/latoheavyitalic.915671b2.woff2"
+  },
+  {
+    "revision": "d46e3712225f8f18b25f18bf90b755fd",
+    "url": "/krasuny-app/static/media/latoheavyitalic.d46e3712.woff"
+  },
+  {
+    "revision": "4b1810180974f499643b285cc9b5aa59",
+    "url": "/krasuny-app/static/media/latoitalic.4b181018.woff2"
+  },
+  {
+    "revision": "62aeca010211d9c6c778ef256937a2bc",
+    "url": "/krasuny-app/static/media/latoitalic.62aeca01.woff"
+  },
+  {
+    "revision": "caf520060ff4926b46bb58d2966f1dfd",
+    "url": "/krasuny-app/static/media/latolight.caf52006.woff2"
+  },
+  {
+    "revision": "f7c88895b1d589f8dee785f727078c5b",
+    "url": "/krasuny-app/static/media/latolight.f7c88895.woff"
+  },
+  {
+    "revision": "b6a98825168d2514fbf4d8554cc7f334",
+    "url": "/krasuny-app/static/media/latolightitalic.b6a98825.woff2"
+  },
+  {
+    "revision": "d412d55aa73a73d3be5af58ac7762823",
+    "url": "/krasuny-app/static/media/latolightitalic.d412d55a.woff"
+  },
+  {
+    "revision": "4a6fa802c6fc2c12b4d2b79d291350be",
+    "url": "/krasuny-app/static/media/latomedium.4a6fa802.woff2"
+  },
+  {
+    "revision": "7fd6bc6bdb0265ed41f84042c916c1d8",
+    "url": "/krasuny-app/static/media/latomedium.7fd6bc6b.woff"
+  },
+  {
+    "revision": "2cf1be2fd8cbf07de036b4e0ab14a377",
+    "url": "/krasuny-app/static/media/latomediumitalic.2cf1be2f.woff"
+  },
+  {
+    "revision": "a021c82f9d2a87f5fd656311d25baa48",
+    "url": "/krasuny-app/static/media/latomediumitalic.a021c82f.woff2"
+  },
+  {
+    "revision": "1fbf86c9bbb08fd76eb28eb50eeeb86a",
+    "url": "/krasuny-app/static/media/latoregular.1fbf86c9.woff"
+  },
+  {
+    "revision": "8201bf32d4c0cf86a54e75b4464caec6",
+    "url": "/krasuny-app/static/media/latoregular.8201bf32.woff2"
+  },
+  {
+    "revision": "44aba5a619630ad15e3b85af5072368f",
+    "url": "/krasuny-app/static/media/latosemibold.44aba5a6.woff2"
+  },
+  {
+    "revision": "9ab55ec42e1a85034ef71af1be4ce8b3",
+    "url": "/krasuny-app/static/media/latosemibold.9ab55ec4.woff"
+  },
+  {
+    "revision": "215d9916804f9d42f732a14f04c9b602",
+    "url": "/krasuny-app/static/media/latosemibolditalic.215d9916.woff2"
+  },
+  {
+    "revision": "f6f0cb8c6fd46013cdf059fd14f431b7",
+    "url": "/krasuny-app/static/media/latosemibolditalic.f6f0cb8c.woff"
+  },
+  {
+    "revision": "46c2ac5b83274f1b94033afb603fcb8a",
+    "url": "/krasuny-app/static/media/latothin.46c2ac5b.woff"
+  },
+  {
+    "revision": "b052ca2329967f94e4021902d973cd55",
+    "url": "/krasuny-app/static/media/latothin.b052ca23.woff2"
+  },
+  {
+    "revision": "7c82ddc3b9296c4db2beeb9664d9928a",
+    "url": "/krasuny-app/static/media/latothinitalic.7c82ddc3.woff2"
+  },
+  {
+    "revision": "94032ce5267d4fbeb40e68e70bc62a7a",
+    "url": "/krasuny-app/static/media/latothinitalic.94032ce5.woff"
+  },
+  {
+    "revision": "7d3c22ed15a9eb77407ec11926be1664",
+    "url": "/krasuny-app/static/media/low-snap--active.7d3c22ed.svg"
+  },
+  {
+    "revision": "5b5931445f0e0e187c904551fc13b051",
+    "url": "/krasuny-app/static/media/low-snap.5b593144.svg"
+  },
+  {
+    "revision": "559e7117a3d34ce7ae53e10f1521be15",
+    "url": "/krasuny-app/static/media/more-snap--active.559e7117.svg"
+  },
+  {
+    "revision": "975740a4726510ec859ba82c21fb8907",
+    "url": "/krasuny-app/static/media/more-snap.975740a4.svg"
+  },
+  {
+    "revision": "4ceaf7076fa30b73be85bcdc332af360",
+    "url": "/krasuny-app/static/media/next-snap.4ceaf707.svg"
+  },
+  {
+    "revision": "4e4c809965ee0080dc6d5b10d80703b8",
+    "url": "/krasuny-app/static/media/popup-consultation-fon.4e4c8099.png"
+  },
+  {
+    "revision": "2a7d71905739d818f4be9692b4cf469c",
+    "url": "/krasuny-app/static/media/popup-programma-fon.2a7d7190.png"
+  },
+  {
+    "revision": "29b4caf490210166bdc6768163103e64",
+    "url": "/krasuny-app/static/media/prev-snap.29b4caf4.svg"
+  },
+  {
+    "revision": "6758906e323e5598bcee3fd3d36cfe1d",
+    "url": "/krasuny-app/static/media/promo-snap.6758906e.svg"
+  },
+  {
+    "revision": "ae6835499eb5f1b16ddf761486c67734",
+    "url": "/krasuny-app/static/media/radio--checked.ae683549.svg"
+  },
+  {
+    "revision": "ec4893b795909b9135d24a28ecdb06e3",
+    "url": "/krasuny-app/static/media/radio--hover.ec4893b7.svg"
+  },
+  {
+    "revision": "35f0ec2c13b2b09bf9bb963aec23cfa0",
+    "url": "/krasuny-app/static/media/radio.35f0ec2c.svg"
+  },
+  {
+    "revision": "3b28d2b0ce586627cde00646081ec0af",
+    "url": "/krasuny-app/static/media/search-icon-tag.3b28d2b0.svg"
+  },
+  {
+    "revision": "5685019f37dce4f4f6f691724ad5f85c",
+    "url": "/krasuny-app/static/media/select-snap--active.5685019f.svg"
+  },
+  {
+    "revision": "6ba29a812555f6d40e4c806b92eeae38",
+    "url": "/krasuny-app/static/media/select-snap--hover.6ba29a81.svg"
+  },
+  {
+    "revision": "53790dabb488f0101be52eb50d0c3536",
+    "url": "/krasuny-app/static/media/select-snap.53790dab.svg"
+  },
+  {
+    "revision": "840d529986aacb341c39bf70d51300c6",
+    "url": "/krasuny-app/static/media/selection-assortment-fon.840d5299.png"
+  },
+  {
+    "revision": "295cece4a8eeb2acc6248bb57f14fe48",
+    "url": "/krasuny-app/static/media/selection-question-fon.295cece4.png"
+  },
+  {
+    "revision": "4a433d9d99e1c58a927c9794d4545280",
+    "url": "/krasuny-app/static/media/validate-icon.4a433d9d.svg"
+  }
+]);
--- src/api/api.ts
+++
@@ -1,4 +0,0 @@
-export function itemsApi (query: string): string {
-  return `http://makeup-api.herokuapp.com/api/v1/products${query}`
-  // return `/`
-}
